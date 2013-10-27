cf2259aec80a51eb41fbfe5fd8b6b2fa81175773
Sponis <sponis@gmail.com>
2013-10-27 05:16:10 +0200

    proxwrame

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+jQuery(document).ready(function(){
+
+	jQuery('#searchform input[type="text"]').on('click',function(){
+		if (jQuery(this).value('Search Keywords')) {
+			jQuery(this).value('');
+		}
+
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
