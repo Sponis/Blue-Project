45429a8b4d17e9cb0b378389b09bcbb039a51921
Sponis <sponis@gmail.com>
2013-11-19 06:09:57 +0200

    version1.1

--- js/main.js
+++ js/main.js
@@ -5,10 +5,20 @@ jQuery(document).ready(function(){
 	twitterFetcher.fetch('398379208813985792', 'example1', 1, true);
 
 	jQuery('#slideshow').cycle({
-		fx: 'fade'
-	});
+		fx: 'fade',
+		timeout:   0,
+        prev:    '#previousbutton',
+        next:    '#nextbutton',
+        pager:   '#pagenavi',
+        pagerAnchorBuilder: pagerFactory
+   	});
 
-		});
+   	function pagerFactory(idx, slide) {
+        var s = idx > 3 ? ' style="display:none"' : '';
+        return '<li'+s+'><a href="#">'+(idx+1)+'</a></li>';
+    };
+
+});
 	
 
 
