4ef1917c38fc67537feb7dec480b2b0f0a822565
Sponis <sponis@gmail.com>
2013-11-22 22:36:58 +0200

    new01

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-//Fetching latest Tweet//
-
 jQuery(document).ready(function(){
 
 	twitterFetcher.fetch('398379208813985792', 'example1', 1, true);
 
+  //slideshow + pagination//
 	jQuery('#slideshow').cycle({
 		fx: 'fade',
 		timeout:   0,
@@ -13,11 +12,19 @@ jQuery(document).ready(function(){
         pagerAnchorBuilder: pagerFactory
    	});
 
+
+    //Fetching latest Tweet//
    	function pagerFactory(idx, slide) {
         var s = idx > 3 ? ' style="display:none"' : '';
         return '<li'+s+'><a href="#">'+(idx+1)+'</a></li>';
     };
 
+
+    //prevent submit form//
+    jQuery('.submitbutton').click(function(event) {
+      event.preventDefault();
+      alert('the form did not submitted');
+    });
 });
 	
 
