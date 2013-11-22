961163a29c5f56c5a15503bab188eb31d08985ef
Sponis <sponis@gmail.com>
2013-11-22 23:22:14 +0200

    new02

--- js/main.js
+++ js/main.js
@@ -21,10 +21,30 @@ jQuery(document).ready(function(){
 
 
     //prevent submit form//
-    jQuery('.submitbutton').click(function(event) {
+    jQuery('.submitbutton').on("click", function(event) {
       event.preventDefault();
-      alert('the form did not submitted');
+      var first_name = jQuery("#first_name").val();
+      console.log(first_name);
+      var last_name = jQuery("#last_name").val();
+      console.log(last_name);
+      var email_from = jQuery("#email").val();
+      console.log(email_from);
+      var telephone = jQuery("#telephone").val();
+      console.log(telephone);
+      var DoB = jQuery("#DoB").val();
+      console.log(DoB);
+      var address = jQuery("#address").val();
+      console.log(address);
+      var country = jQuery("#country").val();
+      console.log(country);
+      var comments = jQuery("#comments").val();
+      console.log(comments);
+      var terms = jQuery("#terms").attr('checked');
+      console.log(terms);
+      console.log('the form did not submitted');
     });
+
+    
 });
 	
 
