0fea2d3cc4baa05d2e8d7d7de0764aaec1ea47b3
Sponis <sponis@gmail.com>
2013-11-24 21:37:37 +0200

    new form

--- js/main.js
+++ js/main.js
@@ -135,9 +135,25 @@ jQuery(document).ready(function(){
 
     });
 
+  //google map api
+  var map;
+  function initialize() {
+    var mapOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(37.956, 23.710)
+    };
+    map = new google.maps.Map(document.getElementById('ggl-map'),
+    mapOptions);
+  } 
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
     
 });
 	
+
+  
 
 
 
